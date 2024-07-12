0d1508f8808b4f42a7d28adbd6af5c9da1633789
rayen <rayenn38@gmail.com>
2024-07-12 17:13:26 +0100

    implements animations

--- lib/screens/meal_detail.dart
+++ lib/screens/meal_detail.dart
@@ -26,17 +26,32 @@ class MealDetail extends ConsumerWidget {
            ScaffoldMessenger.of(context).showSnackBar(
              SnackBar(content: Text(wasAdded ? "add added as a favorite." : "Meal removed.")),
            );
-          }, icon: Icon( isFavorite ? Icons.star : Icons.star_border))
+          }, icon: AnimatedSwitcher(
+            duration: const Duration(milliseconds: 300),
+            transitionBuilder: (child, animation) {
+              return RotationTransition(
+                turns: Tween<double>(begin: 0.5, end: 1.0).animate(animation),
+                child: child,
+              );
+            },
+            child: Icon(
+              isFavorite ? Icons.star : Icons.star_border,
+              key: ValueKey(isFavorite),
+            ),
+          ))
         ],
       ),
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Image.network(
-              meal.imageUrl,
-              height: 300,
-              width: double.infinity,
-              fit: BoxFit.cover,
+            Hero(
+              tag: meal.id,
+              child: Image.network(
+                meal.imageUrl,
+                height: 300,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
             ),
             const SizedBox(height: 14,),
             Text('Ingredients', style: Theme.of(context).textTheme.titleLarge!.copyWith(
